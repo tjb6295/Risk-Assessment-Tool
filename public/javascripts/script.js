29fa1863e2befdc075d36820cfd287683675af73
Thomas Beck <tommy.j.beck1@gmail.com>
2021-04-11 12:57:23 -0400

    initial commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,16 @@
+// credit to https://www.codebrainer.com/blog/contact-form-in-javascript
+class User {
+    constructor(firstName, lastName, discription, state, email, question) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.discription = discription;
+    this.state = state;
+    this.email = email;
+    this.question = question;
+    }
+   }
+
+   function submitAssessment() {
+       //this alert is sent after the send contact button is pushed 
+        alert(firstName.value + " " + "thanks for reaching out, we will review your assessment and get back to you within 48 hours.");
+   }
